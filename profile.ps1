e43ef90f99fa8d97b461422ddf4c78c3efa0cbd8
tkmru <i.am.tkmru@gmail.com>
2017-11-12 01:03:57 +0900

    add alias

--- profile.ps1
+++ profile.ps1
@@ -1,7 +1,9 @@
-function which($name) { Get-Command $name -ErrorAction SilentlyContinue | Select-Object Definition }
-
-Set-Alias time Measure-Command
 Set-Alias g git
 Set-Alias cl clear
 Set-Alias l ls
 ${function:l} = { ls -Force @args }
+${function:up} = { Set-Location .. }
+
+# Missing Bash aliases
+function which($name) { Get-Command $name -ErrorAction SilentlyContinue | Select-Object Definition }
+Set-Alias time Measure-Command
