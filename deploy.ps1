e5d51401882ae7878491c0f09dd61e7844a86842
tkmru <i.am.tkmru@gmail.com>
2017-11-12 01:25:18 +0900

    enable deploy under home dir

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,5 @@
 cp -Force profile.ps1 $profile
+Copy-Item -Path ./home/** -Destination $home -Include **
 
 # Reload Powershell
 $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
